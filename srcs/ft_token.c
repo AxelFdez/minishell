fd2ce6daa80e835f7b61cb984328b0a3056836c2
Christophe Massa <chmassa@c2r4p2.42nice.fr>
2023-03-17 13:30:44 +0100

    save_17/03

--- srcs/ft_token.c
+++ srcs/ft_token.c
@@ -3,7 +3,10 @@
 int	is_close_herringbone(t_parsing *parse)
 {
 	if (parse->input[parse->i] == '>' && parse->input[parse->i +1] == '>')
-			ft_fill_lst(&parse->lst_cmdline, parse, 2, 0);
+	{
+		parse->len = 2;
+		ft_fill_lst(&parse->lst_cmdline, parse, 0);
+	}
 	if ((parse->input[parse->i] == '>' && parse->input[parse->i +2] == '>')
 		|| (parse->input[parse->i] == '>' && parse->input[parse->i -2] == '>'))
 	{
@@ -12,8 +15,11 @@ int	is_close_herringbone(t_parsing *parse)
 	}
 	else if (parse->input[parse->i] == '>' && parse->input[parse->i +1] != '>'
 		&& parse->input[parse->i -1] != '>')
-			ft_fill_lst(&parse->lst_cmdline, parse, 1, 0);
-	// parse->i++;
+		{
+			parse->len = 1;
+			ft_fill_lst(&parse->lst_cmdline, parse, 0);
+			parse->len = 0;
+		}
 	
 	return (1);
 }
@@ -21,7 +27,10 @@ int	is_close_herringbone(t_parsing *parse)
 int	is_open_herringbone(t_parsing *parse)
 {
 	if (parse->input[parse->i] == '<' && parse->input[parse->i +1] == '<')
-			ft_fill_lst(&parse->lst_cmdline, parse, 2, 0);
+	{
+		parse->len = 2;
+		ft_fill_lst(&parse->lst_cmdline, parse, 0);
+	}
 	if ((parse->input[parse->i] == '<' && parse->input[parse->i +2] == '<')
 		|| (parse->input[parse->i] == '<' && parse->input[parse->i -2] == '<'))
 	{
@@ -30,8 +39,11 @@ int	is_open_herringbone(t_parsing *parse)
 	}
 	else if (parse->input[parse->i] == '<' && parse->input[parse->i +1] != '<'
 		&& parse->input[parse->i -1] != '<')
-			ft_fill_lst(&parse->lst_cmdline, parse, 1, 0);
-	// parse->i++;
+		{
+			parse->len = 1;
+			ft_fill_lst(&parse->lst_cmdline, parse, 0);
+		}
+	parse->len = 0;
 	return (1);
 }
 
@@ -43,8 +55,11 @@ int is_pipe(t_parsing *parse)
 		return (0);
 	}
 	if (parse->input[parse->i] == '|')
-			ft_fill_lst(&parse->lst_cmdline, parse, 1, 0);
-	// parse->i++;
+	{
+		parse->len = 1;
+		ft_fill_lst(&parse->lst_cmdline, parse, 0);
+		parse->len = 0;
+	}
 	
 	return (1);
 
