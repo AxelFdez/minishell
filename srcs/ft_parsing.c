9447aac12bf799cc6466189dcc988d9bf6d8cbe1
AxelFdez <axe06@hotmail.fr>
2023-03-18 00:08:28 +0100

    last-changes

--- srcs/ft_parsing.c
+++ srcs/ft_parsing.c
@@ -8,62 +8,62 @@ void	ft_fill_lst(t_list **lst, t_parsing *parse, int start)
 	new = ft_lstnew(ft_substr(parse->input, start, parse->len));
 	ft_lstadd_back(lst, new);
 
-}
+ }
 
-int	is_cmd(int c)
-{
-	if (c == '|' || c == '<' || c == '>')
-		return (1);
-	// if (c >= 32 && c <= 126)
-	// 	return (1);
-	return (0);
-}
+// int	is_cmd(int c)
+// {
+// 	if (c == '|' || c == '<' || c == '>')
+// 		return (1);
+// 	// if (c >= 32 && c <= 126)
+// 	// 	return (1);
+// 	return (0);
+// }
 
-static void	is_string(t_parsing *parse, char c)
-{
-	while (parse->input[parse->i] != c)
-	{
-		parse->i++;
-		parse->len++;
-		ft_fill_lst(&parse->lst_cmdline, parse, parse->i - parse->len);
-		parse->len = 0;
-	}
-}
-
-
-static void	ft_parseur(t_parsing *parse)
-{
-	parse->lst_cmdline = NULL;
-	parse->i = 0;
+// static void	is_string(t_parsing *parse, char c)
+// {
+// 	while (parse->input[parse->i] != c)
+// 	{
+// 		parse->i++;
+// 		parse->len++;
+// 		ft_fill_lst(&parse->lst_cmdline, parse, parse->i - parse->len);
+// 		parse->len = 0;
+// 	}
+// }
 
 
-	parse->len = 0;
-	while (parse->input[parse->i])
-	{
-		if (parse->input[parse->i] == '<' && !is_open_herringbone(parse))
-				return ;
-		else if (parse->input[parse->i] == '>' && !is_close_herringbone(parse))
-				return ;
-		else if (parse->input[parse->i] == '|' && !is_pipe(parse))
-				return ;
-		else if (parse->input[parse->i] == '\"')
-			is_string(parse, '\"');
-		else if (!is_cmd(parse->input[parse->i]))
-		{
-			printf("i = %d\n", parse->i);
-			while (parse->input[parse->i] && !is_cmd(parse->input[parse->i]) && parse->input[parse->i] != ' ')
-			{
-				parse->len++;
-				parse->i++;
-			}
-			printf("len = %d\n", parse->len);
-			ft_fill_lst(&parse->lst_cmdline, parse, parse->i - parse->len);
-			parse->len = 0;
-		}
-			parse->i++;
-	}
-	ft_lstprint_from_head(parse->lst_cmdline);
-}
+// static void	ft_parseur(t_parsing *parse)
+// {
+// 	parse->lst_cmdline = NULL;
+// 	parse->i = 0;
+
+
+// 	parse->len = 0;
+// 	while (parse->input[parse->i])
+// 	{
+// 		if (parse->input[parse->i] == '<' && !is_open_herringbone(parse))
+// 				return ;
+// 		else if (parse->input[parse->i] == '>' && !is_close_herringbone(parse))
+// 				return ;
+// 		else if (parse->input[parse->i] == '|' && !is_pipe(parse))
+// 				return ;
+// 		else if (parse->input[parse->i] == '\"')
+// 			is_string(parse, '\"');
+// 		else if (!is_cmd(parse->input[parse->i]))
+// 		{
+// 			printf("i = %d\n", parse->i);
+// 			while (parse->input[parse->i] && !is_cmd(parse->input[parse->i]) && parse->input[parse->i] != ' ')
+// 			{
+// 				parse->len++;
+// 				parse->i++;
+// 			}
+// 			printf("len = %d\n", parse->len);
+// 			ft_fill_lst(&parse->lst_cmdline, parse, parse->i - parse->len);
+// 			parse->len = 0;
+// 		}
+// 			parse->i++;
+// 	}
+// 	ft_lstprint_from_head(parse->lst_cmdline);
+// }
 
 void	*args_list(t_parsing *parse)
 {
@@ -93,15 +93,119 @@ void	*args_list(t_parsing *parse)
 	return (list_arg);
 }
 
+// char **quotes_args(char *input)
+// {
+// 	int i;
+// 	int j;
+// 	int k;
+// 	char **tab;
+
+// 	i = 0;
+// 	j = 0;
+// 	k = 0;
+// 	tab = malloc(sizeof(tab));
+// 	while (input[i])
+// 	{
+// 		while (input[i] == '"')
+// 		{
+// 			i++;
+// 			j = 0;
+// 			while (input[i] != '"')
+// 			{
+// 				tab[k][j] = input[i];
+// 				j++;
+// 				i++;
+// 			}
+// 			k++;
+// 		}
+// 		i++;
+// 	}
+// 	tab[k + 1][0] = '\0';
+// 	return (tab);
+// }
+
+char **isolate_dquotes(char *input)
+{
+	char **tab;
+	int i;
+	int j;
+	int quotes;
+	int start;
+
+	quotes = 0;
+	i = 0;
+	while (input[i])
+	{
+		if (input[i] == '"')
+			quotes++;
+		i++;
+	}
+	printf("quotes = %d\n", quotes);
+	if (quotes == 0)
+		quotes = 2;
+	tab = malloc(sizeof(char *) * ((quotes / 2) + 1));
+	if (!tab)
+		perror("malloc error");
+	i = 0;
+	j = 0;
+	while (input[i])
+	{
+		if (input[i] != '"')
+		{
+			start = i;
+			while (input[i] != '"')
+			{
+				if (input[i + 1] == '"' || input[i + 1] == '\0')
+					break;
+				i++;
+			}
+			tab[j] = ft_substr(input, start, i - start + 1);
+			j++;
+		}
+		else if (input[i] == '"')
+		{
+			start = i;
+			i++;
+			while (input[i] != '"')
+			{
+				if (input[i] == '"' || input[i] == '\0')
+					break;
+				i++;
+			}
+			printf("start = %d\n", start);
+			printf("i = %d\n", i);
+			tab[j] = ft_substr(input, start, i - start + 1);
+			j++;
+		}
+		i++;
+	}
+	tab[j] = 0;
+	return (tab);
+}
+
 void	ft_get_cmdline(t_parsing *parse)
 {
-	int		i;
+	int		i = 0;
+	char **split_input;
 	parse->lst_cmdline = NULL;
 
 	ft_quotes(parse);
-	i = 0;
+	//i = 0;
 	parse->input = ft_strtrim_free_s1(parse->input, " ");
-	ft_parseur(parse);
+	// decouper args entre quotes et doubles quotes,
+	//split_input = quotes_args(parse->input);
+	split_input = isolate_dquotes(parse->input);
+	// decouper selon les espaces (seulement args sans quotes),
+	// split_input = ft_split(parse->input, ' ');
+	// split_input = ft_split(parse->input, '|');
+	while (split_input[i])
+	{
+		printf("arg[%d] = |%s|\n", i, split_input[i]);
+		i++;
+	}
+	// interpreter ou non les quotes
+	// decouper selon les chevrons ou pipes
+	//ft_parseur(parse);
 	printf("input = %s\n", parse->input);
 	free(parse->input);
 }
