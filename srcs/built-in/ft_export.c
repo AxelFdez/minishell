a68c7cbcfab2de74e06940b7c74765e2a93ecfd2
Christophe Massa <chmassa@c2r4p2.42nice.fr>
2023-03-29 16:54:40 +0200

    save du 29/03

--- srcs/built-in/ft_export.c
+++ srcs/built-in/ft_export.c
@@ -1,78 +1,141 @@
 #include "../../includes/minishell.h"
 
 
-static int	ft_is_exist(t_parsing *parse, t_list *tmp)
-{
-	t_list	*tmp_env;
+// static int	ft_var_is_exist(t_parsing *parse, t_list *tmp)
+// {
 
+// 	t_list	*tmp_env;
+
+// 	tmp_env = parse->lst_env;
+// 	while (tmp_env)
+// 	{
+		
+// 		if (!ft_strncmp(tmp->str, tmp_env->str, ft_strlen(tmp_env->str)))
+// 		{puts("AAAA");
+			
+// 			return (1);
+// 		}
+// 		tmp_env = tmp_env->next;
+// 	}
+// 	return (0);
+// }
+
+
+static int	ft_var_is_exist(t_parsing *parse, t_list *tmp)
+{puts("in ft_var is exist");
+	t_list	*tmp_env;
+	char	*s1;
+	char	*s2;
+
+	s1 = ft_strdup(tmp->str);
+	if (strchr(s1, '='))
+		s1[ft_str_chr(s1, '=')] = '\0';
 	tmp_env = parse->lst_env;
 	while (tmp_env)
 	{
-		if (!ft_strncmp(tmp_env->str, tmp->str,
-				ft_str_chr(tmp_env->str, '=')))
-		{
-			printf("tmp_env->str = %s\n", tmp_env->str);
-			printf("tmp->str = %s\n", tmp->str);
+		s2 = ft_strdup(tmp_env->str);
+		if (strchr(s2, '='))
+			s2[ft_str_chr(s2, '=')] = '\0';
+		if (ft_strcmp(s1, s2) == 0)
+		{puts("MOD VAR");
 			free(tmp_env->str);
 			tmp_env->str = ft_strdup(tmp->str);
+			free(s1);
+			free(s2);
 			return (1);
 		}
+		free(s2);
 		tmp_env = tmp_env->next;
 	}
+	free(s1);
 	return (0);
 }
 
-
-static void	ft_add_node(t_parsing *parse, t_list *tmp)
+static void	ft_add_var(t_parsing *parse, t_list *tmp)
 {
+	puts("in ft_add_var");
 	t_list	*new;
-	
-	printf("tmp->str before if = %s\n", tmp->str);
 
-	if(ft_is_exist(parse, tmp))
-		puts("exist");
-	else{
 
 	new = ft_lstnew(ft_strdup(tmp->str));
 	ft_lstadd_back(&parse->lst_env, new);
-	}
 }
 
-int	ft_export(t_parsing *parse)
-{
-	size_t	i;
-	int		error;
-	// t_list	*new;
-	t_list	*tmp;
+static int	ft_check_var_syntax(char *s)
+{puts("in ft_check var syntax");
+	int	i;
 
 	i = 0;
-	error = 0;
-	tmp = parse->lst_cmdline->next;
-	if (tmp == NULL)
-		ft_print_export(parse);
-	else
+	while (s[i])
 	{
-		while (tmp)
+		if (ft_isalnum(s[i]) || s[i] == '_'
+				|| s[i] == '=' || s[i] == ' ' || s[i] == '\n')
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+static int	ft_new_var(t_parsing *parse, t_list *tmp)
+{puts("in ft_new_var");
+	int	error;
+
+	error = 0;
+	while (tmp)
+	{
+		if (ft_check_var_syntax(tmp->str))
 		{
-			while (tmp->str[i] || ft_isalnum(tmp->str[i]) || tmp->str[i] == '_'
-				|| tmp->str[i] == '=')
-				i++;
-			if (i == ft_strlen(tmp->str))
-			{
-				ft_add_node(parse, tmp);
-				// new = ft_lstnew(ft_strdup(tmp->str));
-				// ft_lstadd_back(&parse->lst_env, new);
-			}
-			else
-			{
-				ft_printf("export: `%s' : not a valid identifier\n", tmp->str);
-				error++;
-			}
-			i = 0;
-			tmp = tmp->next;
+			if (ft_var_is_exist(parse, tmp) == 0)
+				ft_add_var(parse, tmp);
 		}
+		else
+		{
+			ft_printf("export: `%s' : not a valid identifier\n", tmp->str);
+			error++;
+		}
+		tmp = tmp->next;
 	}
 	if (error > 0)
 		return (1);
 	return (0);
+}
+
+int	ft_export(t_parsing *parse)
+{puts("in ft_export");
+	int		error;
+	t_list	*tmp;
+
+	error = 0;
+	tmp = parse->lst_cmdline->next;
+	if (tmp == NULL)
+	{
+		ft_print_export(parse);
+		return (0);
+	}
+	else
+		return (ft_new_var(parse, tmp));
+	// {
+	// 	// while (tmp)
+	// 	// {
+	// 	// 	if (ft_check_var_syntax(tmp->str))
+	// 	// 	{
+	// 	// 		if (!ft_var_is_exist(parse, tmp))
+	// 	// 			ft_add_var(parse, tmp);
+	// 	// 		else
+	// 	// 		{
+
+	// 	// 		}
+	// 	// 	}
+	// 	// 	else
+	// 	// 	{
+	// 	// 		ft_printf("export: `%s' : not a valid identifier\n", tmp->str);
+	// 	// 		error++;
+	// 	// 	}
+	// 	// 	tmp = tmp->next;
+	// 	// }
+	// }
+	// if (error > 0)
+	// 	return (1);
+	// return (0);
 }
