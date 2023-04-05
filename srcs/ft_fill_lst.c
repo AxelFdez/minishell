b266356586b170ce39889ffe38506aea0dc4b507
Chris <chris@MacBook-Pro-de-sdi.local>
2023-04-05 19:47:16 +0200

    save du 5/04

--- srcs/ft_fill_lst.c
+++ srcs/ft_fill_lst.c
@@ -6,12 +6,9 @@ void	ft_fill_lst(t_list **lst, t_parsing *parse, int start)
 	char	*str;
 
 	str = NULL;
-		// printf("str = %s\n", parse->input);
-		// printf("parse->i = %d\n", parse->i);
-	if (parse->str_tmp != NULL)
+	if (parse->str_tmp)
 	{
 		str = ft_strdup(parse->str_tmp);
-
 		free(parse->str_tmp);
 		parse->str_tmp = NULL;
 		str = ft_strjoin_free_s1(str,
@@ -31,7 +28,7 @@ void	ft_fill_lst(t_list **lst, t_parsing *parse, int start)
 		ft_strdel_quotes(parse, str);
 		parse->quote_to_del = 0;
 	}
-	if(str)
+	if (str)
 	{
 		new = ft_lstnew(ft_strdup(str));
 		ft_lstadd_back(lst, new);
