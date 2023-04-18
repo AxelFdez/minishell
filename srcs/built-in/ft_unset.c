1c72c7302cab792d45cba59eec8cb4c2d45547a6
Axel Fernandez <axfernan@c2r12p3.42nice.fr>
2023-04-18 17:36:21 +0200

    last-push

--- srcs/built-in/ft_unset.c
+++ srcs/built-in/ft_unset.c
@@ -14,7 +14,7 @@ void	ft_unset(t_parsing *parse)
 		if (ft_strlen(tmp_cmd->str) == 0)
 		{
 			ft_printf("export: `%s': not a valid identifier\n", tmp_cmd->str);
-			parse->ret_value = 1;
+			sig.return_value = 1;
 		}
 		tmp_env = parse->lst_env;
 		while (tmp_env)
@@ -22,7 +22,10 @@ void	ft_unset(t_parsing *parse)
 			if (!ft_strncmp(tmp_cmd->str, tmp_env->str,
 					ft_str_chr(tmp_env->str, '='))
 						&& (!ft_strnstr(tmp_env->str, tmp_cmd->str, 2)))
-				ft_lstdel_actual(&parse->lst_env, tmp_env);
+		{
+			ft_lstdel_actual(&parse->lst_env, tmp_env);
+			break ;
+		}
 			tmp_env = tmp_env->next;
 		}
 		tmp_cmd = tmp_cmd->next;
